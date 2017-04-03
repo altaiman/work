2df5a407fb8dc3d2a76a0a8018f59a44ad39b063
Roman <altaiman@gmail.com>
2017-04-03 19:34:41 +0700

    Add files via upload

--- impocar/static/js/main.js
+++ impocar/static/js/main.js
@@ -1,8 +1,17 @@
 "use strict";
 "use strict";
 "use strict";
-"use strict";
-"use strict";
+'use strict';
+
+$(function () {
+    $('.content').on('click', function () {
+        var slide = $(this).find('.content__slide');
+
+        if (!slide) return;
+
+        $(slide).toggleClass('content__slide_up');
+    });
+});
 'use strict';
 
 $(function () {
@@ -26,6 +35,7 @@ $(function () {
 "use strict";
 "use strict";
 "use strict";
+"use strict";
 'use strict';
 
 $(function () {
@@ -35,20 +45,22 @@ $(function () {
     function slider() {
         if ($(window).width() < 1000) {
             try {
-                $('.news__list').slick({
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    infinite: false,
-                    prevArrow: $('.news .slider__arrow_prev'),
-                    nextArrow: $('.news .slider__arrow_next'),
-                    responsive: [{
-                        breakpoint: 760,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }]
-                });
+                if ($('.news__list').hasClass('news__list_full')) {} else {
+                    $('.news__list').slick({
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        infinite: false,
+                        prevArrow: $('.news .slider__arrow_prev'),
+                        nextArrow: $('.news .slider__arrow_next'),
+                        responsive: [{
+                            breakpoint: 760,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
+                        }]
+                    });
+                }
             } catch (e) {}
         } else if ($('.news__list').hasClass('slick-slider')) {
             $('.news__list').slick('unslick');
@@ -57,20 +69,6 @@ $(function () {
 });
 "use strict";
 "use strict";
-'use strict';
-
-$(function () {
-    $('.reviews__slider .slider__list').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        variableWidth: true,
-        infinite: false,
-        centerMode: true,
-        initialSlide: 1,
-        prevArrow: $('.reviews__slider .slider__arrow_prev'),
-        nextArrow: $('.reviews__slider .slider__arrow_next')
-    });
-});
 'use strict';
 
 $(function () {
@@ -119,7 +117,20 @@ $(function () {
         $(this).addClass('route_open');
     });
 });
-"use strict";
+'use strict';
+
+$(function () {
+    $('.reviews__slider .slider__list').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        infinite: false,
+        centerMode: true,
+        initialSlide: 1,
+        prevArrow: $('.reviews__slider .slider__arrow_prev'),
+        nextArrow: $('.reviews__slider .slider__arrow_next')
+    });
+});
 'use strict';
 
 $(function () {
@@ -127,4 +138,9 @@ $(function () {
         $('.slide-image li[data-level="1"]').css('width', $(window).width() - e.pageX);
         $('.slide-image li[data-level="1"] img').css('left', -e.pageX);
     });
+
+    if ($(window).width() < 760) {
+        $('iframe').attr('height', '200px');
+    }
 });
+"use strict";
